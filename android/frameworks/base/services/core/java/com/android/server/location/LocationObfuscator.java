ff850107bf502a46d956a24e2b51921704ffee63
JaskaranSidhu88 <jaskaransidhu931@gmail.com>
2025-04-30 08:47:30 +0200

    add generic interface to obfuscate location

---
+++ android/frameworks/base/services/core/java/com/android/server/location/LocationObfuscator.java
@@ -0,0 +1,11 @@
+package com.android.server.location.fudger;
+
+import android.location.Location;
+import android.location.LocationResult;
+
+public interface LocationObfuscator {
+    Location obfuscate(Location fine);
+    default LocationResult obfuscate(LocationResult fineResult) {
+        return fineResult.map(this::obfuscate);
+    }
+}
