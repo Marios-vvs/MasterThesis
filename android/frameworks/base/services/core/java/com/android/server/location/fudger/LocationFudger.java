d834f5fb9bd7cd6af87f30d5e54d661639c367d9
JaskaranSidhu88 <jaskaransidhu931@gmail.com>
2025-04-30 08:50:17 +0200

    Update LocationFudger to implement the interface

--- android/frameworks/base/services/core/java/com/android/server/location/fudger/LocationFudger.java
+++ android/frameworks/base/services/core/java/com/android/server/location/fudger/LocationFudger.java
@@ -35,7 +35,7 @@ import android.util.Log;
  * Contains the logic to obfuscate (fudge) locations for coarse applications. The goal is just to
  * prevent applications with only the coarse location permission from receiving a fine location.
  */
-public class LocationFudger {
+public class LocationFudger implements LocationObfuscator{
 
     // minimum accuracy a coarsened location can have
     private static final float MIN_ACCURACY_M = 200.0f;
